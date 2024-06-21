41588a000e8d5f0cf65a0181638cc31e664170c0
Swechchha <swechchhapandey271@gmail.com>
2024-06-21 23:42:51 +0530

    Showing the suggested users on mobile  screen also

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,25 +1,61 @@
-import { Container, Box, Flex } from "@chakra-ui/react";
+import { Container, Box, Flex, useBreakpointValue } from "@chakra-ui/react";
 import FeedPosts from "../../component/FeedPosts/FeedPosts";
 import SuggestedUsers from "../../component/SuggestedUsers/SuggestedUsers";
 
 const HomePage = () => {
+  // Determine if the current viewport is large or not
+  const isLargeScreen = useBreakpointValue({ base: false, lg: true });
+
   return (
     <Container maxW={"container.lg"}>
-      <Flex gap={20}>
+      <Flex gap={20} direction={{ base: "column", lg: "row" }}>
         <Box flex={2} py={10}>
           <FeedPosts />
         </Box>
-        <Box
-          flex={3}
-          mr={15}
-          display={{ base: "none", lg: "block" }}
-          maxW={"300px"}
-        >
+        {isLargeScreen && (
+          <Box
+            flex={3}
+            mr={15}
+            display={{ base: "none", lg: "block" }}
+            maxW={"300px"}
+          >
+            <SuggestedUsers />
+          </Box>
+        )}
+      </Flex>
+      {!isLargeScreen && (
+        <Box mt={10}>
           <SuggestedUsers />
         </Box>
-      </Flex>
+      )}
     </Container>
   );
 };
 
 export default HomePage;
+
+// import { Container, Box, Flex } from "@chakra-ui/react";
+// import FeedPosts from "../../component/FeedPosts/FeedPosts";
+// import SuggestedUsers from "../../component/SuggestedUsers/SuggestedUsers";
+
+// const HomePage = () => {
+//   return (
+//     <Container maxW={"container.lg"}>
+//       <Flex gap={20}>
+//         <Box flex={2} py={10}>
+//           <FeedPosts />
+//         </Box>
+//         <Box
+//           flex={3}
+//           mr={15}
+//           display={{ base: "row", lg: "block" }}
+//           maxW={"300px"}
+//         >
+//           <SuggestedUsers />
+//         </Box>
+//       </Flex>
+//     </Container>
+//   );
+// };
+
+// export default HomePage;
